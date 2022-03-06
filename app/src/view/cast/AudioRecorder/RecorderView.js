f0dbe0354f840a3db3bb57359e6426f13902df89
Quietscher <76123025+Quietscher@users.noreply.github.com>
2022-03-06 18:38:07 +0100

    updated audio recording

--- app/src/view/cast/AudioRecorder/RecorderView.js
+++ app/src/view/cast/AudioRecorder/RecorderView.js
@@ -1,12 +1,12 @@
 /* eslint-env browser */
 
-import {Observable, Event} from "../../../utils/Observable.js";
+import { Observable, Event } from "../../../utils/Observable.js";
 
-var iconLight, 
-    title, 
-    time, 
-    iconStart, 
-    iconSend,
+var iconLight,
+    title,
+    time,
+    iconStart,
+    iconStop,
     startTime,
     timerInterval,
     audioLength;
@@ -18,66 +18,143 @@ class Recorder extends Observable {
         this.initUI();
     }
 
-    initUI(){
+    initUI() {
         iconLight = document.querySelector(".recorder-icon-light");
         title = document.querySelector(".recorder-title");
         time = document.querySelector(".recorder-time");
         iconStart = document.querySelector(".recorder-icon-start");
-        iconSend = document.querySelector(".recorder-icon-send");
-        iconStart.addEventListener("click",this.onStartRecording.bind(this));
-        iconSend.addEventListener("click", this.onSendRecording.bind(this));
+        iconStop = document.querySelector(".recorder-icon-stop");
+        this.iconSave = document.querySelector(".recorder-icon-save");
+        this.iconSave.addEventListener("click", this.onSaveRecordingClicked.bind(this));
+        iconStart.addEventListener("click", this.onStartRecording.bind(this));
+        iconStop.addEventListener("click", this.onStopRecordingClicked.bind(this));
+        this.iconTrash = document.querySelector(".recorder-icon-trash");
+        this.iconTrash.addEventListener("click", this.onTrashClicked.bind(this));
     }
-  
-    onStartRecording(){
+
+    onStopRecordingClicked() {
+        this.showIconTrash();
+        this.hideIconStop();
+        clearInterval(timerInterval);
+        let data = {
+                title: title.value,
+                time: audioLength,
+            },
+            event = new Event("stop-recording", data);
+        this.notifyAll(event);
+    }
+
+    onTrashClicked() {
+        this.hideIconSave();
+        this.hideIconTrash();
+        this.hideIconStop();
+        this.showIconMic();
+        stopTimer();
+        this.notifyAll(new Event("delete-recording", "currentRec"));
+    }
+
+    onSaveRecordingClicked() {
+        console.log("Recording stops");
+        stopTimer();
+        this.hideIconSave();
+        this.hideIconTrash();
+        this.showIconMic();
+
+        // toggleIconLight();
+        let data = {
+                title: title.value,
+                time: audioLength,
+            },
+            event = new Event("save-recording", data);
+        this.notifyAll(event);
+        title.value = "";
+        audioLength = "00:00";
+    }
+
+    onStartRecording() {
         console.log("Recording starts");
+        this.hideIconMic();
+        this.showIconStop();
         startTimer();
-        toggleIconLight();
+        // toggleIconLight();
         let event = new Event("start-recording", "data");
         this.notifyAll(event);
     }
-  
-    onSendRecording(){
-        console.log("Recording stops");
-        stopTimer();
-        toggleIconLight();
-        let data = {
-            title: title.value,
-            time: audioLength,
-        },
-            event = new Event("send-recording", data);
-        this.notifyAll(event);
-        title.value = "";
+
+    getTitle() {
+        return title.value;
     }
-  
+
+    getAudioTime() {
+        return audioLength;
+    }
+
+    hideIconSave() {
+        this.iconSave.classList.add("hidden");
+    }
+
+    showIconSave() {
+        this.iconSave.classList.remove("hidden");
+    }
+
+    hideIconStop() {
+        iconStop.classList.add("hidden");
+    }
+
+    showIconStop() {
+        iconStop.classList.remove("hidden");
+    }
+
+    hideIconMic() {
+        iconStart.classList.add("hidden");
+    }
+
+    showIconMic() {
+        iconStart.classList.remove("hidden");
+    }
+
+    hideIconTrash() {
+        this.iconTrash.classList.add("hidden");
+    }
+
+    showIconTrash() {
+        this.iconTrash.classList.remove("hidden");
+    }
+
+    stopTimer() {
+        stopTimer();
+    }
+
 }
 
 //Starts the timer, stores the start time and shows the current length of the recording audio file
-function startTimer(){
+function startTimer() {
     startTime = Date.now();
-    timerInterval = setInterval(()=>{
+    timerInterval = setInterval(() => {
         let currentTime = Date.now(),
             formatter = new Intl.NumberFormat('de-DE', { minimumIntegerDigits: 2 }),
             minutes,
             seconds;
-        audioLength = Math.floor((currentTime - startTime) /1000);
-        minutes = formatter.format(Math.floor(audioLength/60));
+        audioLength = Math.floor((currentTime - startTime) / 1000);
+        minutes = formatter.format(Math.floor(audioLength / 60));
         seconds = formatter.format(audioLength % 60);
         audioLength = minutes + ":" + seconds;
         time.innerHTML = audioLength;
-    },1000);
+    }, 1000);
 }
 
 //Stops the timer and showing the time in the UI
-function stopTimer(){
+function stopTimer() {
     clearInterval(timerInterval);
     time.innerHTML = "00:00";
 }
+
 //Turns the Lightbulb on/off
-function toggleIconLight(){
-    if(iconLight.style.background !== "lemonchiffon"){
+function toggleIconLight() {
+    if (iconLight.style.background !== "lemonchiffon") {
         iconLight.style.borderRadius = "999px";
         iconLight.style.background = "lemonchiffon";
-    } else {iconLight.style.background = " ";}
+    } else { iconLight.style.background = " "; }
 }
 
 export default Recorder;
