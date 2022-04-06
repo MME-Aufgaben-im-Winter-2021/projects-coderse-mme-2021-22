9b1cd121dc552981f78e12657664ea3ee29572ab
EmmaSophieReichert <emmasophie1203@gmx.de>
2022-04-06 17:50:28 +0200

    Kommentarergänzungen

--- app/src/api/User/updateUser.js
+++ app/src/api/User/updateUser.js
@@ -1,16 +1,16 @@
 import appwrite from "../appwrite.js";
 
-// Update the username from the currently logged user
+// Update the username from the currently logged in user
 function updateUsername(username) {
     let promise = appwrite.account.updateName(username);
     return promise;
 }
-// Update the password from the currently logged user
+// Update the password from the currently logged in user
 function updatePassword(password) {
     let promise = appwrite.account.updatePassword(password);
     return promise;
 }
-// Update the email from the currently logged user
+// Update the email from the currently logged in user
 function updateEmail(email, password) {
     let promise = appwrite.account.updateEmail(email, password);
     return promise;
