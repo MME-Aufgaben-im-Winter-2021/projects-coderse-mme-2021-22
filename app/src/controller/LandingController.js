115392611f6084a74474fa876dd713f9dcb0e435
Quietscher <76123025+Quietscher@users.noreply.github.com>
2022-05-27 13:46:38 +0200

    first landing page prototype

---
+++ app/src/controller/LandingController.js
@@ -0,0 +1,23 @@
+/* eslint-env browser */
+
+import { Observable, Event } from "../utils/Observable.js";
+
+class LandingController extends Observable {
+
+    init(navView) {
+        this.navView = navView;
+        this.navView.hideLinks();
+        this.navView.hideSafeBtn();
+        this.navView.hideTitleInput();
+        this.navView.hideNavView();
+
+        this.loginBtn = document.querySelector(".lan-btn-login");
+        this.loginBtn.addEventListener("click", this.notifyAll.bind(this, new Event("on-login-clicked", "login")));
+
+        this.signUpBtn = document.querySelector(".lan-btn-sign-up");
+        this.signUpBtn.addEventListener("click", this.notifyAll.bind(this, new Event("on-sign-up-clicked",
+            "register")));
+    }
+}
+
+export default LandingController;
